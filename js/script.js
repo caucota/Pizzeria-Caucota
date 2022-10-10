284cb6166722d2eb4568935544ab9935a4fef885
Federico Caucota <caucota@gmail.com>
2022-10-10 20:41:28 -0300

    preentrega2

--- js/script.js
+++ js/script.js
@@ -1,43 +1,183 @@
-/*
-let primerMensaje = "Ingrese el numero del producto que desea comprar: ";
-let listaProductos = "\n1- Pizzas\n2- Empanadas\n3- Lomitos\n4- Finalizar Compra";
-let opcionCancelar = "\n5- Cancelar Compra";
-let cicloCompra = true;
 
 
-function Productos (tipo, nombre, precio) {
+class Persona{
+    constructor(apellido, nombre, dni){
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+    set Apellido(apellido){
+        this.apellido = apellido;
+    }
+    set Nombre(nombre){
+        this.nombre = nombre;
+    }
+    set Dni(dni){
+        this.dni = dni;
+    }
+}
+
+class Cliente extends Persona{
+    constructor(apellido, nombre, dni, domicilio, telefono){
+        super(apellido, nombre, dni);
+        this.domicilio = domicilio;
+        this.telefono  = telefono;
+    }
+    set Telefono(telefono){
+        this.telefono = telefono;
+    }
+    set Domicilio(domicilio){
+        this.domicilio = domicilio;
+    }
+}
+
+
+class metodoPago{
+    constructor(tipo, nroTarjeta, codigoSeguridad, fechaVencimiento, nombre){
+        this.tipo = tipo;
+        this.nroTarjeta = nroTarjeta;
+        this.codigoSeguridad = codigoSeguridad;
+        this.fechaVencimiento = fechaVencimiento;
+        this.nombre = nombre
+    }
+}
+function Producto (id, tipo, nombre, precio, stock) {
+    this.id = id;
     this.tipo = tipo;
     this.nombre = nombre;
     this.precio = precio;
+    this.stock = stock||0;
 }
 
+class opcionMenu{
+    constructor(id, nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+    tieneStock(){
+        let idxTieneStock = arrayProductos.findIndex(prod=> (prod.tipo == this.id) && (prod.stock > 0) );
+        return (idxTieneStock !== -1);
+    }
+};
+const opcionMenuPizzas = new opcionMenu(1, "Pizza");
+const opcionMenuEmpanadas = new opcionMenu(2, "Empanadas");
+const opcionMenuLomitos = new opcionMenu(3, "Lomitos");
+const opcionMenuFinalizarCompra = new opcionMenu(4, "Finalizar Compra")
+const opcionMenuCancelarCompra = new opcionMenu(5, "Cancelar Compra")
 
-function SumarPedido(productoElegido){
-    let cantidadProducto = prompt ("Ingrese la cantidad de " + productoElegido.tipo + " " + productoElegido.nombre + " " + "que desea comprar: ")
-    if (articulosPedidos != "")
-    {
-        articulosPedidos = articulosPedidos +"\n";
+let opcionesMenu = [];
+opcionesMenu.push(opcionMenuPizzas);
+opcionesMenu.push(opcionMenuEmpanadas);
+opcionesMenu.push(opcionMenuLomitos);
+
+function menuTiposProductos(){
+    let listaTiposProductos = "";
+    if (productosSeleccionados.length != 0){
+        listaTiposProductos = "¿Desea agregar algún otro producto a su compra?\n";
     };
-    articulosPedidos = articulosPedidos + cantidadProducto + " " + productoElegido.tipo + " " + productoElegido.nombre;
-    precioTotal = precioTotal + (cantidadProducto * productoElegido.precio);
-    return primerPrecio + precioTotal;
+    listaTiposProductos = listaTiposProductos.concat("Ingrese el numero del producto que desea comprar: ");
+
+    for (const opcMenu of opcionesMenu){
+        let conStock = opcMenu.tieneStock();
+        listaTiposProductos = listaTiposProductos.concat("\n", opcMenu.id, "-", opcMenu.nombre)
+        if (!conStock){
+            listaTiposProductos = listaTiposProductos.concat("(Sin Stock)")
+        }
+    };
+
+    listaTiposProductos = listaTiposProductos.concat("\n", opcionMenuFinalizarCompra.id, "-", opcionMenuFinalizarCompra.nombre)
+    if(productosSeleccionados.length > 0){
+        listaTiposProductos = listaTiposProductos.concat("\n", opcionMenuCancelarCompra.id, "-", opcionMenuCancelarCompra.nombre)
+    }
+    listaTiposProductos = listaTiposProductos.concat("\n Total:$" , totalCompra());
+    return listaTiposProductos;
 }
 
-let pizzaA = new Productos("Pizza", "Muzzarela", 800)
-let pizzaB = new Productos("Pizza", "Napolitana", 850)
-let empanadaA = new Productos("Empanadas", "Arabes", 50)
-let empanadaB = new Productos("Empanadas", "Jamón y Queso", 70)
-let lomitoA = new Productos("Lomito", "Vegetariano", 900)
-let lomitoB = new Productos("Lomito", "Común", 800)
+let arraySubMenu = [];
+function menuProductosSegunTipo(idTipoProducto){
+    let submenuProductos = 'Elige entre nuestras opciones:';
+    let arraySegunTipoProducto = arrayProductos.filter((prod)=>prod.tipo == idTipoProducto  && prod.stock > 0);
+    if (arraySegunTipoProducto.length > 0){
+        submenuProductos = submenuProductos.concat("\n0-Menu Anterior");
+        arraySubMenu = [];
+        let contador = 0;
+        for( prod of arraySegunTipoProducto){
+            contador +=1;
+            submenuProductos = submenuProductos.concat("\n", contador, "-", prod.nombre , " : $", prod.precio );
+            let prodSubMenu = new Producto(prod.id, idTipoProducto, prod.nombre, 0);
+            arraySubMenu.push(prodSubMenu);
+        }
+    }else{
+        submenuProductos = 'Opción incorrecta.';
+        submenuProductos = submenuProductos.concat("\n0-Menu Anterior");
+    }
+    return submenuProductos;
+}
 
-let segundoMensaje = "Elige entre nuestras opciones: ";
-let precioTotal = 0;
-let articulosPedidos = "";
-let mensajeFinal = "En total serían: $";
-let primerPrecio = "Hasta ahora son: $";
+let productosSeleccionados = [];
 
+
+/*
+class Compra{
+    constructor(cliente, metodoPago){
+        this.cliente = new Cliente(cliente.apellido, cliente.nombre, cliente.dni, cliente.dni, cliente.domicilio, cliente.telefono);
+        this.metodoPago = metodoPago
+    }
+}
+*/
+function SumarPedido(productoElegido){
+    if (productoElegido != null){
+        let tipoProd = opcionesMenu.find((op)=>op.id == productoElegido.tipo);
+        let cantidadProducto = prompt ("Ingrese la cantidad de " + tipoProd.nombre + " " + productoElegido.nombre + " " + "que desea comprar (stock = " + productoElegido.stock + ") : " )
+        
+        if (cantidadProducto <= productoElegido.stock){
+
+            productoElegido.stock = productoElegido.stock - cantidadProducto;
+
+            let seleccionAnterior = productosSeleccionados.some(otroProducto => otroProducto.id === productoElegido.id)
+            if (seleccionAnterior == true){
+                productosSeleccionados.forEach ((otroProducto) =>{
+                    if(otroProducto.id == productoElegido.id){
+                        otroProducto.cantidad = parseInt(otroProducto.cantidad) + parseInt(cantidadProducto);
+                        otroProducto.total = parseInt(otroProducto.cantidad) * otroProducto.precio;
+                        return;
+                    }
+                })
+            }else{
+                productosSeleccionados.push({id: productoElegido.id, tipo: productoElegido.tipo, nombre: productoElegido.nombre, precio:productoElegido.precio, cantidad: parseInt(cantidadProducto), total:(cantidadProducto * productoElegido.precio)});
+            }
+        }else{
+            alert("Stock insuficiente");
+        }
+    }else{
+        alert("Error al seleccionar el Producto");
+    }
+
+};
+
+
+function totalCompra(){
+    return productosSeleccionados.reduce((acumulador, elemento)=>acumulador + elemento.total, 0);
+}
+function resumenCompra(){
+    let resumenPedido = "Resumen de su Pedido:";
+    productosSeleccionados.forEach((prodCompra) =>{
+        let tipoProd = opcionesMenu.find((op)=>op.id == prodCompra.tipo);
+        resumenPedido = resumenPedido.concat("\n",prodCompra.cantidad, " " , tipoProd.nombre, " ", prodCompra.nombre, " :$", prodCompra.total);
+    });
+    resumenPedido = resumenPedido.concat("\nEn total serían: $", totalCompra());
+    alert(resumenPedido);
+}
+
+let pizzaA = new Producto(1, 1, "Muzzarella", 800, 10)
+let pizzaB = new Producto(2, 1, "Napolitana", 850, 5)
+let empanadaA = new Producto(3, 2, "Arabes", 50, 11)
+let empanadaB = new Producto(4, 2, "Jamón y Queso", 70)
+let lomitoA = new Producto(5, 3, "Vegetariano", 900, 0)
+let lomitoB = new Producto(6, 3, "Común", 800, 0)
 
 let arrayProductos = [pizzaA, pizzaB, empanadaA, empanadaB, lomitoA, lomitoB];
+/*
 let catalogo = document.getElementById("catalogo");
 
 for (const prod of arrayProductos){
@@ -46,87 +186,39 @@ for (const prod of arrayProductos){
     card.innerHTML = `<h2 class="tituloCard">Nombre: ${prod.nombre}</h2><p>Precio: $${prod.precio}</p>`
     catalogo.append(card);
 }
-
+*/
+let cicloCompra = true;
 while(cicloCompra) {
-    let productoElegido = prompt (primerMensaje + listaProductos + "\n Total:$" + precioTotal);
-    switch (productoElegido){
-        case "1":
-            let productoElegidoPizza = prompt (segundoMensaje + "\n1-" + pizzaA.nombre + "\n2-" + pizzaB.nombre + "\n3-Menu Anterior"  );
-            switch (productoElegidoPizza) {
-                case "1":{
-                    alert(SumarPedido(pizzaA));
-                    break;
-                }
-                case "2":{
-                    alert(SumarPedido(pizzaB));
-                    break;
-                }
-                default: {
-                    break;
-                }
-            }
-            break;
-
-        case "2" :
-            let productoElegidoEmpanada = prompt (segundoMensaje + "\n1-" + empanadaA.nombre + "\n2-" + empanadaB.nombre+ "\n3-Menu Anterior");
-            switch (productoElegidoEmpanada){
-                case "1":{
-                    alert(SumarPedido(empanadaA));
-                    break;
-                }
-                case "2":{
-                    alert(SumarPedido(empanadaB));
-                    break;
-                }
-                default: {
-                    break;
-                }
-            }
-            break;
-
-        case "3":
-            let productoElegidoLomito = prompt (segundoMensaje + "\n1-" + lomitoA.nombre + "\n2-" + lomitoB.nombre+ "\n3-Menu Anterior");
-            switch (productoElegidoLomito){
-                case "1":{
-                    alert(SumarPedido(lomitoA));
-                    break;
-                }
-                case "2":{
-                    alert(SumarPedido(lomitoB));
-                    break;
-                }
-                default: {
-                    break;
-                }
-            }
-            break;
-        case "4":{
-            cicloCompra = false;
-            break;
-        }
-        default:
-        {
-            precioTotal = 0
-            cicloCompra = false;
-            break;
-        }
+    let tipoProductoElegido = prompt(menuTiposProductos());
+    if (tipoProductoElegido == "4"){
+        break;
     }
-    primerMensaje = "¿Desea agregar algún otro producto a su compra?\nIngrese el numero del que desea comprar: ";
-    if (precioTotal > 0){
-        listaProductos = listaProductos + opcionCancelar;
-        opcionCancelar= "";
+    if (tipoProductoElegido == "5"){
+        productosSeleccionados.splice(0, productosSeleccionados.length);
+        break;
     }
+    let productoElegidoPizza = prompt(menuProductosSegunTipo(tipoProductoElegido));
+    if (productoElegidoPizza == "0"){
+        continue;
+    }
+    if ((productoElegidoPizza <= arraySubMenu.length) && (productoElegidoPizza > 0)){
+        let productoSubMenu = arraySubMenu[parseInt(productoElegidoPizza-1)];
+        let productoAAgregar = arrayProductos.find((prod)=>prod.id == productoSubMenu.id);
+        SumarPedido(productoAAgregar);
+    }
+    else
+        alert("Seleccione una opción válida");
 }
 
-console.log(precioTotal);
-if (precioTotal > 0) {
-    alert("Resumen de su Pedido:\n" +articulosPedidos + "\n" +mensajeFinal + " " + precioTotal);
+
+if (productosSeleccionados.length > 0) {
+    resumenCompra();
 
     let cicloDatos = true
     let nombreCliente = "";
     let apellidoCliente = "";
     let direccionCliente = "";
-
+    
     while (cicloDatos){
         nombreCliente = prompt("Ingrese su Nombre: ");
         apellidoCliente = prompt("Ingrese su Apellido: ");
@@ -138,6 +230,7 @@ if (precioTotal > 0) {
         }
         cicloDatos = false;
     }
+    let cliente = new Cliente(apellidoCliente, nombreCliente, null, null, null);
     
     cicloDatos = true;
     
@@ -151,6 +244,7 @@ if (precioTotal > 0) {
         }
         cicloDatos = false;
     }
+    cliente.Domicilio = direccionCliente;
     
     let mensajeFormaPago = "Ingrese la forma de pago:";
     let opcionesFormaPago = "\n1- En efectivo\n2- Transferencia";
@@ -161,113 +255,14 @@ if (precioTotal > 0) {
         opcionesFormaPago = "Transferencia";
     }
     
-    alert("Gracias por tu compra " + nombreCliente);
-    //document.getElementById("totalAPagarInput").value = "$" + precioTotal;
-    document.getElementById("totalAPagarInput").innerText = "$" + precioTotal;
+    alert("Gracias por tu compra " + cliente.nombre);
+    document.getElementById("totalAPagarInput").value = "$" + totalCompra();
     document.getElementById("formPagoInput").value = opcionesFormaPago;
-    document.getElementById("nombreInput").value = nombreCliente;
-    document.getElementById("apellidoInput").value = apellidoCliente;
-    document.getElementById("domicilioInput").value = direccionCliente;
+    document.getElementById("nombreInput").value = cliente.nombre;
+    document.getElementById("apellidoInput").value = cliente.apellido;
+    document.getElementById("domicilioInput").value = cliente.domicilio;
 
 } else {
     alert("Gracias por tu visita. Te esperamos..")
 }
-    
-*/
 
-
-//TIENDA
-
-let precioTotal = 0
-let totalFinal = 0
-
-function Producto(nombre,precio,stock){
-    this.nombre = nombre;
-    this.precio = precio;
-    this.stock = stock;
-    this.restarStock = function(cantidad){ // recibe como parametro la cantidad, creo metodo para actualizar stock
-        this.stock -= cantidad
-    }
-}
-
-function calculadoraPrecioTotal (cantidadReloj,reloj){
-    precioTotal = cantidadReloj * reloj
-    totalFinal = totalFinal  + precioTotal
-    alert("El precio total es $" + precioTotal)
-}
-
-let reloj1 = new Producto("Reloj Automatico",100,5)
-let reloj2 = new Producto("Reloj a Cuerda",200,5)
-let reloj3 = new Producto("Reloj con Calendario",300,5)
-let reloj4 = new Producto("Reloj con Segundero",400,5)
-
-let listaProductos = [reloj1,reloj2,reloj3,reloj4]
-let resumenCompra = []
-
-let cantidadCompra = prompt("Hola, ingrese la cantidad de productos que desea que comprar!!")
-
-for(let i = 0; i < cantidadCompra; i++){
-
-    let reloj = prompt("Seleccione un producto: \n1 - Reloj Automatico\n2 - Reloj a Cuerda\n3 - Reloj con Calendario\n4 - Reloj con Segundero\n5 - Salir")
-
-    while(reloj != 5){
-        if(reloj == 1 ){
-            let cantidadReloj = prompt("¿Cuantos " + reloj1.nombre + " desea comprar?")
-            if (cantidadReloj <= reloj1.stock){
-                calculadoraPrecioTotal (cantidadReloj,reloj1.precio)
-                // reloj1.restarStock(reloj1.stock)
-                resumenCompra.push(reloj1.precio * cantidadReloj)
-                break;
-            }
-            else{
-                alert("No tenemos suficiente stock, la cantidad de stock es de " + reloj1.stock + " unidades.")
-            }
-        }
-        else if (reloj == 2 ){
-            let cantidadReloj = prompt("¿Cuantos " + reloj2.nombre + " desea comprar?")
-            if (cantidadReloj <= reloj2.stock){
-                calculadoraPrecioTotal(cantidadReloj,reloj2.precio)
-                // reloj2.restarStock(reloj2.stock)
-                resumenCompra.push(reloj2.precio * cantidadReloj)
-                break;
-            }
-            else{
-                alert("No tenemos suficiente stock, la cantidad de stock es de " + reloj2.stock + " unidades.")
-            }
-        }
-        else if (reloj == 3 ){
-            let cantidadReloj = prompt("¿Cuantos " + reloj3.nombre + " desea comprar?")
-            if (cantidadReloj <= reloj3.stock){
-                calculadoraPrecioTotal(cantidadReloj,reloj3.precio)
-                // reloj3.restarStock(reloj3.stock)
-                resumenCompra.push(reloj3.precio * cantidadReloj)
-                break;
-            }
-            else{
-                alert("No tenemos suficiente stock, la cantidad de stock es de " + reloj3.stock + " unidades.")
-            }
-        }
-        else if (reloj == 4 ){
-            let cantidadReloj = prompt("¿Cuantos " + reloj4.nombre + " desea comprar?")
-            if (cantidadReloj <= reloj4.stock){
-                calculadoraPrecioTotal(cantidadReloj,reloj4.precio)
-                // reloj4.restarStock(reloj4.stock)
-                resumenCompra.push(reloj4.precio * cantidadReloj)
-                break;
-            }
-            else{
-                alert("No tenemos suficiente stock, la cantidad de stock es de " + reloj4.stock + " unidades.")
-            }
-        }
-        else{
-            alert("Ingrese una opcion valida")
-            break;
-        }
-    }
-}
-totalFinal = 0;
-for(const resumen of resumenCompra){
-	totalFinal = totalFinal + resumen;
-}
-alert("El total final a pagar a pagar es de: $" + totalFinal)
-alert("Gracias por su compra")
